8346fb7737d3e4953b1fcd15f67e8187ee90c7dc
Peter Vais <peter-vais@hotmail.sk>
2021-09-23 08:18:13 +0200

    Form1 and FormReview2 added

---
+++ WinFormApplication/WinFormApplication/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿
+namespace WinFormApplication
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbName = new System.Windows.Forms.Label();
+            this.lbDesignation = new System.Windows.Forms.Label();
+            this.lbSalary = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbName
+            // 
+            this.lbName.AutoSize = true;
+            this.lbName.Location = new System.Drawing.Point(84, 53);
+            this.lbName.Name = "lbName";
+            this.lbName.Size = new System.Drawing.Size(41, 13);
+            this.lbName.TabIndex = 0;
+            this.lbName.Text = "Name: ";
+            // 
+            // lbDesignation
+            // 
+            this.lbDesignation.AutoSize = true;
+            this.lbDesignation.Location = new System.Drawing.Point(84, 79);
+            this.lbDesignation.Name = "lbDesignation";
+            this.lbDesignation.Size = new System.Drawing.Size(69, 13);
+            this.lbDesignation.TabIndex = 1;
+            this.lbDesignation.Text = "Designation: ";
+            // 
+            // lbSalary
+            // 
+            this.lbSalary.AutoSize = true;
+            this.lbSalary.Location = new System.Drawing.Point(84, 105);
+            this.lbSalary.Name = "lbSalary";
+            this.lbSalary.Size = new System.Drawing.Size(42, 13);
+            this.lbSalary.TabIndex = 2;
+            this.lbSalary.Text = "Salary: ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(759, 451);
+            this.Controls.Add(this.lbSalary);
+            this.Controls.Add(this.lbDesignation);
+            this.Controls.Add(this.lbName);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbName;
+        private System.Windows.Forms.Label lbDesignation;
+        private System.Windows.Forms.Label lbSalary;
+    }
+}
+
---
+++ WinFormApplication/WinFormApplication/FormReview2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormApplication
+{
+    public partial class FormReview2 : Form
+    {
+        public FormReview2()
+        {
+            InitializeComponent();
+        }
+    }
+}
